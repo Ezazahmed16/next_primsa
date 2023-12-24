5683a774c20800a3c8fba964b2deeb813ed262c0
Ezaz Ahmed <ezazrahul794@gmail.com>
2023-12-24 10:58:41 +0600

    next_prisma

---
+++ prisma/migrations/20231224045531_registration/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(50) NOT NULL,
+    `last_name` VARCHAR(50) NOT NULL,
+    `age` INTEGER NOT NULL,
+    `grade` VARCHAR(20) NOT NULL,
+    `courses` JSON NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
